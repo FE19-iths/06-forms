97c266a2219127b118d7a00470edcf0bf7b5af6c
David Andersson <lejonmanen@gmail.com>
2020-06-01 16:05:42 +0200

    Validering

--- src/components/BookForm.jsx
+++ src/components/BookForm.jsx
@@ -1,15 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState/*, useEffect*/ } from 'react';
 import './BookForm.css';
 
 const BookForm = () => {
     const [title, setTitle] = useState('');
     const [author, setAuthor] = useState('');
+    const [titleTouched, setTitleTouched] = useState(false);
+    const [authorTouched, setAuthorTouched] = useState(false);
 
     const stopSubmit = event => {
         event.preventDefault();
     }
 
-    useEffect(() => {
+    /*useEffect(() => {
         // om det är tomt i input-fälten, men finns saker i localStorage - hämta data från localStorage och lägg i input-fälten
         // annars -> spara det användaren skrivit i localStorage
         if( title === '' && author === '' ) {
@@ -21,7 +23,14 @@ const BookForm = () => {
         } else {
             localStorage.setItem('library.book', JSON.stringify({ title, author }));
         }
-    }, [title, author])
+    }, [title, author])*/
+
+    let [titleClass, titleError] = titleTouched
+        ? isValidTitle(title)
+        : ['', ''];
+    let [authorClass, authorError] = authorTouched
+        ? isValidAuthor(author)
+        : ['', ''];
 
     // First input is an UNCONTROLLED component
     // Second input is CONTROLLED
@@ -32,16 +41,24 @@ const BookForm = () => {
             <div className="form-group">
                 <label> Book title </label>
                 <input type="text" placeholder="The book title"
-                    onChange={e => setTitle(e.target.value)} />
+                    className={titleClass}
+                    onChange={e => setTitle(e.target.value)}
+                    onBlur={() => setTitleTouched(true)} />
+                <div className="error">{titleError}</div>
             </div>
+
             <div className="form-group">
                 <label> Author name </label>
                 <input type="text" placeholder="The author's name"
+                    className={authorClass}
                     onChange={e => setAuthor(e.target.value)}
+                    onBlur={() => setAuthorTouched(true)}
                     value={author} />
+                <div className="error">{authorError}</div>
             </div>
+
             <div className="form-group">
-                <button> Register book in database </button>
+                <button disabled={titleError || authorError}> Register book in database </button>
             </div>
             <div>
                 If you click, "{title}" by "{author}" will be added.
@@ -49,6 +66,24 @@ const BookForm = () => {
             </form>
         </div>
     )
+}
+
+function isValidTitle(title) {
+    if( String(title) !== '' ) {
+        return ['valid', ''];
+    } else {
+        return ['invalid', 'Please enter a title']
+    }
+}
+function isValidAuthor(author) {
+    let validAuthor = typeof(author) === 'string'
+        && isNaN(Number(author));
+
+    if( validAuthor ) {
+        return ['valid', ''];
+    } else {
+        return ['invalid', "Please enter the author's name"]
+    }
 }
 
 export default BookForm;
